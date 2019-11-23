8869a6a4c85d13e577f6e1ca940baef0cabf69fd
Dimple Patel <dimplep@hotmail.com>
2019-11-23 10:23:59 -0500

    started main screen layout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    $(document).ready(function () {
+//        alert("hello world");
+});
